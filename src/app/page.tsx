53cca2d671302ec3f1f1dc1e2d8d81ffefd5bed7
howard-rose <philip.anthon@gmail.com>
2024-04-13 23:58:03 +0800

    feat: add reflection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,30 @@ export default function Home() {
               <p>two!</p>
             </TabPanel>
             <TabPanel>
-              <p>three!</p>
+              <Text as='b'>
+                (Masid-Danas) How was my experience going through SAS3000 and what were my observations and self-discoveries?
+              </Text>
+              <Text>
+                Overall, I can say that my experience going through SAS3000 was eye-opening. Going in, I felt wholly unprepared for entering a career, and it was worrying since our practicum would be coming up soon. However, bit by bit, I was able to see and equip myself with the tools I needed to take my first steps in entering the career world. I realized that maybe, career hunting isn&apos;t as scary as I thought it was after all. 
+              </Text>
+              <Text as='b'>
+                (Suri-Nilay) How does my own knowledge, values, attitudes and assumptions of past and present experiences influence the way I feel or act on my career pursuit?
+              </Text>
+              <Text>
+                For me, I had already decided since graduating high school that I was set on choosing a career path involved in IT or Computer Science. This goal that I set for myself served as my guide to the choices that I would make ever since. However, I was also really clueless about what the industry was actually like, and I think that hindered me from trying to seek out more opportunities for me to grow and learn. Now, I feel like I know better, and I want to better myself.
+              </Text>
+              <Text as='b'>
+                (Taya-Kilos) What would I like to change and improve about myself?
+              </Text>
+              <Text>
+                I know that I have a lot of weaknesses; the first of which is my indecisiveness or lack of initiative. There are times where I just don&apos;t know what the right decision is. Sometimes, I feel too scared or worried about what may happen if I fail to do something, and I just end up not doing it. I want to be more confident when it comes to new experiences, and know that even if I fail, I will still have grown as a person. I also want to become a more outgoing and social person, as I know that having social skills is very important in the career world, being just as important or even more important than raw technical skill.
+              </Text>
+              <Text as='b'>
+                (Ramdam-Pakiramdam) What are  the implications of my actions for my career plans, myself and others?
+              </Text>
+              <Text>
+                With all that I&apos;ve learned, I feel more confident in my plans and what actions I should take to further those plans moving forward. Hopefully, I can find a place where I can feel contented with my career, and know that I am making a positive impact on the world somehow.
+              </Text>
             </TabPanel>
           </TabPanels>
         </Tabs>
